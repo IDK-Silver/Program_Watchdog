c009e4aa8cdee8a4bf85a7d960dd9e6bbb38d44a
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2023-01-11 15:39:18 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,51 @@
-#include <iostream>
 #include <PID_Controller/PID_Controller.h>
+#include <ArgumentsDecode/ArgumentsDecode.h>
+#include <string>
+#include <vector>
+#include <memory>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char *argv[])
 {
-    int count;
+    /* the program arguments */
+    std::vector<std::string> arguments;
 
-    // Display each command-line argument.
-    std::cout << "\nCommand-line arguments:\n";
-    for( count = 0; count < argc; count++ )
-        std::cout << "  argv[" << count << "]   "
-             << argv[count] << "\n";
+    /* the pid data */
+    auto data = std::make_shared<PID_Controller_Data>();
 
-//    PID_Controller process_watchdog;
-//    process_watchdog.SetPID(22584);
-//
-//    if (process_watchdog.IsProcess())
-//    {
-//        std::cout << "Terminate Process";
-//        process_watchdog.Terminate();
-//    }
-//    else
-//    {
-//        std::cout << "Can't Terminate Process";
-//    }
+    /* the controller */
+    PID_Controller controller;
+
+    /* copy argv to arguments */
+    for (int count = 0; count < argc; count++)
+        arguments.emplace_back(argv[count]);
+
+    /* decode the arguments */
+    ArgumentsDecode::decode(data, arguments);
+
+    /* set the main program pid */
+    controller.SetPID(data->main_program_pid);
+
+    ArgumentsDecode::print_data(*data);
+
+    /* sleep program unit the main program is not running */
+    while (controller.IsProcess())
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    /* print message */
+    std::cout << "The main program is not running\n";
+
+    /* terminate all watch program */
+    for (const auto& pid : data->watch_pids)
+    {
+        std::cout << "terminate pid : " << pid << "\n";
+        PID_Controller terminate_controller;
+        terminate_controller.SetPID(pid);
+        terminate_controller.Terminate();
+    }
+
     return 0;
 }
